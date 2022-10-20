3fa03191ef5eb3b168339f4986a16a8fb3ac2847
PanosKontos <panos96el@gmail.com>
2022-10-20 22:07:01 +0300

    first commit

---
+++ pages/[slug].js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Main from '../components/main-layout'
+
+export default function Home() {
+  return (
+  <Main>
+
+    
+
+
+    </Main>
+    
+
+  
+  )
+}
